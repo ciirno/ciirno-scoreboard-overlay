7c9bf130fd783ab329ecb4f79b05c7df5eaa80aa
Kendrell Derek Acibar <59480053+ciirno@users.noreply.github.com>
2025-10-03 19:17:27 +0800

    Update home.tsx

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,5 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { Scoreboard } from "../scoreboard/scoreboard";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -17,5 +17,5 @@ export function loader({ context }: Route.LoaderArgs) {
 }
 
 export default function Home({ loaderData }: Route.ComponentProps) {
-  return <Welcome message={loaderData.message} />;
+  return <Scoreboard />;
 }
